90e6e4bf5b45d94e51a75d2c270c4be581dc7c3a
dizafebriyan <diza.febriyan@gmail.com>
2022-11-06 04:45:37 +0800

    resnet, belum sempurna

--- src/model_list.py
+++ src/model_list.py
@@ -22,26 +22,27 @@ def model_selection(name, pretrained):  # TODO: Add more models
         num_classes = 2
         if pretrained:
             net = torchvision.models.detection.retinanet_resnet50_fpn(weights="COCO_V1", trainable_backbone_layers=0)
+            in_features = net.head.classification_head.conv[0][0].in_channels
+            num_anchors = net.head.classification_head.num_anchors
+            net.head.classification_head.num_classes = num_classes
+
+            cls_logits = nn.Conv2d(
+                in_features,
+                num_anchors * num_classes,
+                kernel_size=3,
+                stride=1,
+                padding=1,
+            )
+            nn.init.normal_(cls_logits.weight, std=0.01)
+            nn.init.constant_(cls_logits.bias, -math.log((1 - 0.01) / 0.01))
+
+            net.head.classification_head.cls_logits = cls_logits
+
         else:
-            net = torchvision.models.detection.retinanet_resnet50_fpn(num_classes=2, trainable_backbone_layers=0)
-
-        in_features = net.head.classification_head.conv[0][0].in_channels
-        num_anchors = net.head.classification_head.num_anchors
-        net.head.classification_head.num_classes = num_classes
-
-        cls_logits = nn.Conv2d(
-            in_features,
-            num_anchors * num_classes,
-            kernel_size=3,
-            stride=1,
-            padding=1,
-        )
-        nn.init.normal_(cls_logits.weight, std=0.01)
-        nn.init.constant_(cls_logits.bias, -math.log((1 - 0.01) / 0.01))
-
-        net.head.classification_head.cls_logits = cls_logits
+            net = torchvision.models.detection.retinanet_resnet50_fpn_v2(num_classes=2, trainable_backbone_layers=0)
 
         return net
+
 
     elif name == "ssdlite":
         net = torchvision.models.detection.ssdlite320_mobilenet_v3_large(num_classes=2, trainable_backbone_layers=0)
